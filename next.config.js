5922aeb64cfe5b977dc379dc0f29eeb9070c93a2
Felix Koppe <felix.koppe@cavorit.de>
2026-04-01 14:20:01 +0200

    fixit

--- next.config.js
+++ next.config.js
@@ -4,14 +4,4 @@ module.exports = {
   trailingSlash: false,
   assetPrefix: process.env.ASSET_PREFIX,
   basePath: process.env.BASE_PATH,
-  experimental: {
-    webpackBuildWorker: true,
-  },
-  webpack(config, context) {
-    config.module.rules.push({
-      test: /\.txt$/,
-      use: "raw-loader",
-    });
-    return config;
-  },
 };
